39b83d7be9db275f3c3ef4bd95cff0fc2e512647
Max Komov <64292028+Rus-Bear@users.noreply.github.com>
2021-02-13 14:57:51 +0300

    Added navbar layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,21 +5,21 @@ const NavBar = () => {
     return (
         <div className="navBar">
             <div className="navBar__searchBlock">
-                <img alt="social-logo" src="https://avatars.mds.yandex.net/get-zen_doc/1534997/pub_5d73a5de86c4a900ae934969_5d73b34c4735a600aee9cb4f/scale_1200" />
-                <input type="search" />
+                <img alt="social-logo" src="assets/img/logo.png" />
+                <i className="fas fa-search"></i><input type="search" placeholder="Найти в Lacis" />
             </div>
             <div className="navBar__mainBlock">
-                <a href="#" >Ссылка</a>
-                <a href="#" >Ссылка</a>
-                <a href="#" >Ссылка</a>
-                <a href="#" >Ссылка</a>
-                <a href="#" >Ссылка</a>
+                <a data-tooltip="Новости" className="active" href="#" ><i className="fas fa-newspaper"></i></a>
+                <a data-tooltip="Чат" href="#" ><i className="fas fa-comments"></i></a>
+                <a data-tooltip="Видеочат" href="#" ><i className="fas fa-video"></i></a>
+                <a data-tooltip="Расписание" href="#" ><i className="fas fa-stream"></i></a>
+                <a data-tooltip="Погода" href="#" ><i className="fas fa-cloud-sun"></i></a>
             </div>
             <div className="navBar__appsBlock">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
+                <button className="navBar__appsBlock_imgButton" ><img alt="profileImg" src="https://sun2-10.userapi.com/s/v1/if1/HthZ29x_WWEFYFTTC2C7DhBw-ahsMrkwE8zsrqHU8pqmA1l8lLFGINNlcQh9vB7RLNW6Hmiq.jpg?size=50x0&quality=96&crop=173,173,1382,1382&ava=1" /><p>Максим</p></button>
+                <button className="navBar__appsBlock_iconButton" data-tooltip="Группа" ><i className="fas fa-users"></i></button>
+                <button className="navBar__appsBlock_iconButton" data-tooltip="Ссылки" ><i className="fas fa-link"></i></button>
+                <button className="navBar__appsBlock_iconButton" data-tooltip="Настройки" ><i className="fas fa-sliders-h"></i></button>
             </div>
         </div>
     );
